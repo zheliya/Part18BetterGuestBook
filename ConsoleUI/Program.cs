05cc8ef5a1cf9cf1093aa1b75fca07260944b9ea
zheli <zheli@ZHELIYA>
2024-06-08 23:48:18 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GuestBookLibrary.Models;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<GuestModel> guests = new List<GuestModel>();
+            string noMoreGuestsComing = "";
+
+            do
+            {
+                GuestModel guest = new GuestModel();
+                Console.Write("What is your first name: ");
+                guest.FirstName = Console.ReadLine();
+
+                Console.Write("What is your last name: ");
+                guest.LastName = Console.ReadLine();
+
+                Console.Write("What message would you like to tell your host: ");
+                guest.MessageToHost = Console.ReadLine();
+
+                Console.Write("Are more guests coming (yes/no): ");
+                noMoreGuestsComing = Console.ReadLine();
+
+                guests.Add(guest);
+
+                Console.Clear();
+            } while (noMoreGuestsComing.ToLower() != "no");
+
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
